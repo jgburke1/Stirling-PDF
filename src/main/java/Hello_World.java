fa3988ec748c76084c5758aee3efde6f3e6fae17
jgburke1 <jackburke0812@gmail.com>
2023-11-06 12:39:37 -0500

    Create Hello_World.java
    I created this class to make my mark on the file by stating my name.

---
+++ src/main/java/Hello_World.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "jack";
+		System.out.println(name);
+	}
+
+}
